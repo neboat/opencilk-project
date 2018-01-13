a3fe80e15ed7cec34ea57a830e286fc0a100e94e
TB Schardl <neboat@mit.edu>
2018-01-13 05:40:03 +0000

    Bug fixes for initial rebase onto version 6.

--- clang/include/clang/Serialization/ASTReader.h
+++ clang/include/clang/Serialization/ASTReader.h
@@ -1023,6 +1023,7 @@ private:
   struct PragmaAlignPackStackEntry {
     Sema::AlignPackInfo Value;
     SourceLocation Location;
+    SourceLocation PushLocation;
     StringRef SlotLabel;
   };
   llvm::SmallVector<PragmaAlignPackStackEntry, 2> PragmaAlignPackStack;

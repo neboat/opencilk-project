ef8c75d041c544815b1b04b9d5ef9b32eb6a29fb
TB Schardl <neboat@mit.edu>
2018-02-09 14:08:21 +0000

    [Tapir] Added a new invokable intrinsic, detached_rethrow.  This intrinsic serves to separate exception-handling code in a detached task from exception-handling code in the detacher.  The detached_rethrow intrinsic identifies a landing-pad desintation in the detacher that will catch an exception thrown by a detached task.  Just as the reattach instruction models the ordinary return from a detached task, a detached_rethrow models an exception return, i.e., a resume, from a detached task.  Whereas the reattach instruction identifies the normal continuation from its corresponding detach, a detached_rethrow (roughly speaking) identifies the unwind destination of the corresponding detach.

--- llvm/lib/IR/Verifier.cpp
+++ llvm/lib/IR/Verifier.cpp
@@ -5366,10 +5366,11 @@ void Verifier::visitInstruction(Instruction &I) {
                 F->getIntrinsicID() == Intrinsic::experimental_gc_statepoint ||
                 F->getIntrinsicID() == Intrinsic::wasm_throw ||
                 F->getIntrinsicID() == Intrinsic::wasm_rethrow ||
+                F->getIntrinsicID() == Intrinsic::detached_rethrow ||
                 IsAttachedCallOperand(F, CBI, i),
             "Cannot invoke an intrinsic other than donothing, patchpoint, "
-            "statepoint, coro_resume, coro_destroy, clang.arc.attachedcall or "
-            "wasm.(re)throw",
+            "statepoint, coro_resume, coro_destroy, detached_rethrow, "
+            "clang.arc.attachedcall or wasm.(re)throw",
             &I);
       Check(F->getParent() == &M, "Referencing function in another module!", &I,
             &M, F, F->getParent());

f41480060768e94a512a17f80eb9aee3c9803c7b
TB Schardl <neboat@mit.edu>
2018-10-09 03:18:17 +0000

    [Tapir] Reorganizing Tapir and Clang options to match semantics.  Passing Tapir target for lowering via TargetLibraryInfo.

--- llvm/include/llvm/Frontend/Driver/CodeGenOptions.h
+++ llvm/include/llvm/Frontend/Driver/CodeGenOptions.h
@@ -13,6 +13,8 @@
 #ifndef LLVM_FRONTEND_DRIVER_CODEGENOPTIONS_H
 #define LLVM_FRONTEND_DRIVER_CODEGENOPTIONS_H
 
+#include "llvm/Transforms/Tapir/TapirTargetIDs.h"
+
 namespace llvm {
 class Triple;
 class TargetLibraryInfoImpl;
@@ -34,7 +36,8 @@ enum class VectorLibrary {
 };
 
 TargetLibraryInfoImpl *createTLII(llvm::Triple &TargetTriple,
-                                  VectorLibrary Veclib);
+                                  VectorLibrary Veclib,
+                                  TapirTargetID TapirTarget);
 
 } // end namespace llvm::driver
 

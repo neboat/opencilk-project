2465ed1648e0ff9fc93f5ad42ba41c7065876ddd
TB Schardl <neboat@mit.edu>
2018-10-09 03:18:17 +0000

    [Tapir] Reorganizing Tapir and Clang options to match semantics.  Passing Tapir target for lowering via TargetLibraryInfo.

--- clang/include/clang/Basic/LangOptions.h
+++ clang/include/clang/Basic/LangOptions.h
@@ -471,9 +471,6 @@ public:
   /// forward slash (/) elsewhere.
   bool UseTargetPathSeparator = false;
 
-  /// Runtime target for Tapir.
-  TapirTargetID TapirTarget = TapirTargetID::Last_TapirTargetID;
-
   LangOptions();
 
   /// Set language defaults for the given input language and
--- clang/lib/CodeGen/BackendUtil.cpp
+++ clang/lib/CodeGen/BackendUtil.cpp
@@ -278,6 +278,9 @@ static TargetLibraryInfoImpl *createTLII(llvm::Triple &TargetTriple,
   default:
     break;
   }
+
+  TLII->setTapirTarget(CodeGenOpts.getTapirTarget());
+
   return TLII;
 }
 

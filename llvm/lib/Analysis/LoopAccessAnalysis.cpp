d1106ae52ce7c86c5a99f1619569d801e7253512
TB Schardl <neboat@mit.edu>
2018-10-09 16:16:06 +0000

    [LAA] Fixing LoopAccessAnalysis to ignore Tapir instructions.

--- llvm/lib/Analysis/LoopAccessAnalysis.cpp
+++ llvm/lib/Analysis/LoopAccessAnalysis.cpp
@@ -2471,6 +2471,11 @@ bool LoopAccessInfo::analyzeLoop(AAResults *AA, const LoopInfo *LI,
             !hasPointerArgs(Call) && !VFDatabase::getMappings(*Call).empty())
           continue;
 
+        // TODO: Determine if we should do something other than ignore Tapir
+        // instructions here.
+        if (isa<DetachInst>(&I) || isa<ReattachInst>(&I) || isa<SyncInst>(&I))
+          continue;
+
         auto *Ld = dyn_cast<LoadInst>(&I);
         if (!Ld) {
           recordAnalysis("CantVectorizeInstruction", Ld)
@@ -2495,6 +2500,11 @@ bool LoopAccessInfo::analyzeLoop(AAResults *AA, const LoopInfo *LI,
 
       // Save 'store' instructions. Abort if other instructions write to memory.
       if (I.mayWriteToMemory()) {
+        // TODO: Determine if we should do something other than ignore Tapir
+        // instructions here.
+        if (isa<DetachInst>(&I) || isa<ReattachInst>(&I) || isa<SyncInst>(&I))
+          continue;
+
         auto *St = dyn_cast<StoreInst>(&I);
         if (!St) {
           recordAnalysis("CantVectorizeInstruction", St)

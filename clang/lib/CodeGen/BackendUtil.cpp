fffe392fffc4b89fee1cfe2ded17f7a8d3ed21cb
TB Schardl <neboat@mit.edu>
2018-10-31 23:32:51 +0000

    [CodeGen] Add support to the new pass manager for compiling Tapir programs to a specified runtime.

--- clang/lib/CodeGen/BackendUtil.cpp
+++ clang/lib/CodeGen/BackendUtil.cpp
@@ -86,6 +86,7 @@
 #include "llvm/Transforms/Scalar/GVN.h"
 #include "llvm/Transforms/Scalar/JumpThreading.h"
 #include "llvm/Transforms/Tapir/TapirTargetIDs.h"
+#include "llvm/Transforms/Tapir/TapirToTarget.h"
 #include "llvm/Transforms/Utils/Debugify.h"
 #include "llvm/Transforms/Utils/ModuleUtils.h"
 #include <memory>
@@ -1035,7 +1036,8 @@ void EmitAssemblyHelper::RunOptimizationPipeline(
     } else if (PrepareForLTO) {
       MPM.addPass(PB.buildLTOPreLinkDefaultPipeline(Level));
     } else {
-      MPM.addPass(PB.buildPerModuleDefaultPipeline(Level));
+      MPM.addPass(PB.buildPerModuleDefaultPipeline(Level, false,
+                                                   TLII->hasTapirTarget()));
     }
   }
 

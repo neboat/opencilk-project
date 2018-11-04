c7216944560c9552bdf42fc954a7273590823532
TB Schardl <neboat@mit.edu>
2018-11-04 16:07:13 -0500

    Bug fixes for rebase onto release_70

--- llvm/lib/Transforms/Utils/CodeExtractor.cpp
+++ llvm/lib/Transforms/Utils/CodeExtractor.cpp
@@ -969,6 +969,8 @@ Function *CodeExtractor::constructFunctionDeclaration(
       case Attribute::MustProgress:
       case Attribute::NoProfile:
       case Attribute::SkipProfile:
+      case Attribute::SanitizeCilk:
+      case Attribute::Stealable:
         break;
       // These attributes cannot be applied to functions.
       case Attribute::Alignment:

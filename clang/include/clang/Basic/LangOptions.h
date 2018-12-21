ad893f7e78850990683ec2b6bf668900dcb3245d
TB Schardl <neboat@mit.edu>
2018-12-21 05:08:42 +0000

    [CSI] Add compiler flag to specify when the CSI pass should insert instrumentation during the optimization pipeline.

--- clang/include/clang/Basic/LangOptions.h
+++ clang/include/clang/Basic/LangOptions.h
@@ -442,6 +442,18 @@ public:
     CX_None
   };
 
+  enum class CSIExtensionPoint {
+    // Don't run CSI
+    CSI_None = 0,
+    // The following extension points should be consistent with the extension
+    // points allowed by the pass manager, except for EnabledOnOptLevel0.
+    CSI_EarlyAsPossible,
+    CSI_ModuleOptimizerEarly,
+    CSI_OptimizerLast,
+    CSI_TapirLate,
+    CSI_TapirLoopEnd
+  };
+
   // Define simple language options (with no accessors).
 #define LANGOPT(Name, Bits, Default, Description) unsigned Name : Bits;
 #define ENUM_LANGOPT(Name, Type, Bits, Default, Description)

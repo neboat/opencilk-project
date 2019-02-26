dfcf2ea5da2d664fa0cc98ec6c4584f51d5ad3cf
TB Schardl <neboat@mit.edu>
2019-02-26 12:37:25 +0000

    [CodeGen] Fix code generation for several cases where _Cilk_spawn is used to spawn a complex initialization for a variable.

--- clang/lib/CodeGen/CGExprComplex.cpp
+++ clang/lib/CodeGen/CGExprComplex.cpp
@@ -50,11 +50,19 @@ class ComplexExprEmitter
   bool IgnoreReal;
   bool IgnoreImag;
   bool FPHasBeenPromoted;
+  bool DoSpawnedInit = false;
+  LValue LValueToSpawnInit;
 
 public:
   ComplexExprEmitter(CodeGenFunction &cgf, bool ir = false, bool ii = false)
       : CGF(cgf), Builder(CGF.Builder), IgnoreReal(ir), IgnoreImag(ii),
         FPHasBeenPromoted(false) {}
+
+  ComplexExprEmitter(CodeGenFunction &cgf, LValue LValueToSpawnInit,
+                     bool ir = false, bool ii = false)
+      : CGF(cgf), Builder(CGF.Builder), IgnoreReal(ir), IgnoreImag(ii),
+        FPHasBeenPromoted(false), DoSpawnedInit(true),
+        LValueToSpawnInit(LValueToSpawnInit) {}
 
   //===--------------------------------------------------------------------===//
   //                               Utilities
@@ -131,7 +139,18 @@ public:
   }
   ComplexPairTy VisitCilkSpawnExpr(CilkSpawnExpr *CSE) {
     CGF.PushDetachScope();
-    return Visit(CSE->getSpawnedExpr());
+    ComplexPairTy C = Visit(CSE->getSpawnedExpr());
+    if (DoSpawnedInit) {
+      assert(CGF.CurDetachScope && CGF.CurDetachScope->IsDetachStarted() &&
+             "Processing _Cilk_spawn of expression did not produce detach.");
+      LValue LV = LValueToSpawnInit;
+      EmitStoreOfComplex(C, LV, /*init*/ true);
+
+      // Pop the detach scope
+      CGF.IsSpawned = false;
+      CGF.PopDetachScope();
+    }
+    return C;
   }
 
   ComplexPairTy emitConstant(const CodeGenFunction::ConstantEmission &Constant,
@@ -1504,6 +1523,10 @@ void CodeGenFunction::EmitComplexExprIntoLValue(const Expr *E, LValue dest,
                                                 bool isInit) {
   assert(E && getComplexType(E->getType()) &&
          "Invalid complex expression to emit");
+  if (IsSpawned && isInit) {
+    ComplexExprEmitter(*this, dest).Visit(const_cast<Expr*>(E));
+    return;
+  }
   ComplexExprEmitter Emitter(*this);
   ComplexPairTy Val = Emitter.Visit(const_cast<Expr*>(E));
   Emitter.EmitStoreOfComplex(Val, dest, isInit);

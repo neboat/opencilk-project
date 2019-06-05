0de39f673fe27663c30dd5c98ecd20f440bd8d4e
TB Schardl <neboat@mit.edu>
2019-06-05 20:35:19 +0000

    Bug fixes for rebase onto version 8.

--- clang/lib/CodeGen/CGExpr.cpp
+++ clang/lib/CodeGen/CGExpr.cpp
@@ -635,6 +635,7 @@ EmitMaterializeTemporaryExpr(const MaterializeTemporaryExpr *M) {
     default:
       break;
     }
+    }
     EmitAnyExprToMem(E, Object, Qualifiers(), /*IsInit*/true);
   }
   pushTemporaryCleanup(*this, M, E, Object);

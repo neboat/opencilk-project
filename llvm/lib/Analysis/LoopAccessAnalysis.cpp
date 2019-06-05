30ccf3a0e3a9b6b1fa3b68094975d6f67021ae04
TB Schardl <neboat@mit.edu>
2019-06-05 20:51:20 +0000

    Bug fixes for rebase onto version 8.

--- llvm/lib/Analysis/LoopAccessAnalysis.cpp
+++ llvm/lib/Analysis/LoopAccessAnalysis.cpp
@@ -2484,8 +2484,7 @@ bool LoopAccessInfo::analyzeLoop(AAResults *AA, LoopInfo *LI,
             !VFDatabase::getMappings(*Call).empty())
           continue;
 
-        // TODO: Determine if we should do something other than ignore Tapir
-        // instructions here.
+        // Ignore Tapir instructions.
         if (isa<DetachInst>(&I) || isa<ReattachInst>(&I) || isa<SyncInst>(&I))
           continue;
 

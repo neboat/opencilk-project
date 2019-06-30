adb9c3b2c9a118f20ad493927f15091fb50b3a97
TB Schardl <neboat@mit.edu>
2019-06-30 11:32:02 +0000

    [CSI] Add CSI option to disable promotion of calls to invokes, in order to simplify support for languages with no exceptions.

--- llvm/include/llvm/Transforms/Utils/Instrumentation.h
+++ llvm/include/llvm/Transforms/Utils/Instrumentation.h
@@ -170,7 +170,8 @@ struct SanitizerCoverageOptions {
 };
 
 // Insert CilkSanitizer (Cilk determinacy race detection) instrumentation
-ModulePass *createCilkSanitizerLegacyPass(bool JitMode = false);
+ModulePass *createCilkSanitizerLegacyPass(bool JitMode = false,
+                                          bool CallsMayThrow = true);
 
 // Options for comprehensive static instrumentation
 struct CSIOptions {
@@ -186,6 +187,7 @@ struct CSIOptions {
   bool Interpose = true;
 
   bool jitMode = false;
+  bool CallsMayThrow = true;
 
   CSIOptions() = default;
 };

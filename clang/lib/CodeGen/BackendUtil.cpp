1976e81dbc2b1fbd11b8a505d922133275f8a796
TB Schardl <neboat@mit.edu>
2020-01-12 13:52:42 +0000

    Bug fixes for LLVM 9 rebase

--- clang/lib/CodeGen/BackendUtil.cpp
+++ clang/lib/CodeGen/BackendUtil.cpp
@@ -870,6 +870,7 @@ void EmitAssemblyHelper::RunOptimizationPipeline(
   PTO.LoopInterleaving = CodeGenOpts.UnrollLoops;
   PTO.LoopVectorization = CodeGenOpts.VectorizeLoop;
   PTO.SLPVectorization = CodeGenOpts.VectorizeSLP;
+  PTO.LoopStripmine = CodeGenOpts.StripmineLoop;
   PTO.MergeFunctions = CodeGenOpts.MergeFunctions;
   // Only enable CGProfilePass when using integrated assembler, since
   // non-integrated assemblers don't recognize .cgprofile section.
@@ -1127,8 +1128,8 @@ void EmitAssemblyHelper::RunOptimizationPipeline(
     } else if (PrepareForLTO) {
       MPM.addPass(PB.buildLTOPreLinkDefaultPipeline(Level));
     } else {
-      MPM.addPass(PB.buildPerModuleDefaultPipeline(Level, false,
-                                                   TLII->hasTapirTarget()));
+      MPM.addPass(PB.buildPerModuleDefaultPipeline(
+          Level, /* LTOPreLink */ false, TLII->hasTapirTarget()));
     }
   }
 

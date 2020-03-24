d50432fe8e938229dc23f97165a411a3f758bc2e
TB Schardl <neboat@mit.edu>
2020-03-24 13:17:25 +0000

    [TaskCanonicalize] Add pass to canonicalize taskframes, specifically, to split basic blocks at taskframe.create intrinsics.

--- llvm/lib/Passes/PassBuilder.cpp
+++ llvm/lib/Passes/PassBuilder.cpp
@@ -319,6 +319,7 @@
 #include "llvm/Transforms/Utils/StripGCRelocates.h"
 #include "llvm/Transforms/Utils/StripNonLineTableDebugInfo.h"
 #include "llvm/Transforms/Utils/SymbolRewriter.h"
+#include "llvm/Transforms/Utils/TaskCanonicalize.h"
 #include "llvm/Transforms/Utils/TaskSimplify.h"
 #include "llvm/Transforms/Utils/UnifyFunctionExitNodes.h"
 #include "llvm/Transforms/Utils/UnifyLoopExits.h"

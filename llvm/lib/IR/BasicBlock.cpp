8d914de0437eabbee606e71950d9d9538a791563
TB Schardl <neboat@mit.edu>
2020-04-23 22:00:20 +0000

    [BasicBlock] Add helper method to skip sync.unwind intrinsics at the start of a basic block.

--- llvm/lib/IR/BasicBlock.cpp
+++ llvm/lib/IR/BasicBlock.cpp
@@ -390,6 +390,21 @@ BasicBlock::getFirstNonPHIOrDbgOrLifetime(bool SkipPseudoOp) const {
   return end();
 }
 
+const Instruction* BasicBlock::getFirstNonPHIOrDbgOrSyncUnwind() const {
+  for (const Instruction &I : *this) {
+    if (isa<PHINode>(I) || isa<DbgInfoIntrinsic>(I))
+      continue;
+
+    if (auto *CB = dyn_cast_or_null<CallBase>(&I))
+      if (const Function *Called = CB->getCalledFunction())
+        if (Intrinsic::sync_unwind == Called->getIntrinsicID())
+          continue;
+
+    return &I;
+  }
+  return nullptr;
+}
+
 BasicBlock::const_iterator BasicBlock::getFirstInsertionPt() const {
   const_iterator InsertPt = getFirstNonPHIIt();
   if (InsertPt == end())

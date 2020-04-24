e0a140fd5b4a52a635e060d835aa469879654978
TB Schardl <neboat@mit.edu>
2020-04-24 14:12:25 +0000

    [InlineFunction] Treat taskframe.create intrinsics as defining entry blocks, where allocas and syncregion.start intrinsics in callees are placed in the caller during inlining.

--- llvm/lib/Transforms/Utils/InlineFunction.cpp
+++ llvm/lib/Transforms/Utils/InlineFunction.cpp
@@ -2755,6 +2755,12 @@ llvm::InlineResult llvm::InlineFunction(CallBase &CB, InlineFunctionInfo &IFI,
   return Ret;
 }
 
+static bool isTaskFrameCreate(const Instruction &I) {
+  if (const IntrinsicInst *II = dyn_cast<IntrinsicInst>(&I))
+    return Intrinsic::taskframe_create == II->getIntrinsicID();
+  return false;
+}
+
 static BasicBlock *SplitResume(ResumeInst *RI, Intrinsic::ID TermFunc,
                                Value *Token, BasicBlock *Unreachable) {
   Value *RIValue = RI->getValue();
@@ -2960,6 +2966,11 @@ llvm::InlineResult llvm::InlineFunction(CallBase &CB, InlineFunctionInfo &IFI,
     }
   }
 
+  // Canonicalize the caller by splitting blocks containing taskframe.create
+  // intrinsics.
+  if (splitTaskFrameCreateBlocks(*Caller))
+    OrigBB = CB.getParent();
+
   // Determine if we are dealing with a call in an EHPad which does not unwind
   // to caller.
   bool EHPadForCallUnwindsLocally = false;
@@ -2977,10 +2988,9 @@ llvm::InlineResult llvm::InlineFunction(CallBase &CB, InlineFunctionInfo &IFI,
   // we move allocas from the inlined code, we must move them to this block.
   BasicBlock *DetachedCtxEntryBlock;
   {
-    BasicBlock *CallingBlock = CB.getParent();
-    DetachedCtxEntryBlock = GetDetachedCtx(CallingBlock);
-    assert(((&(CallingBlock->getParent()->getEntryBlock()) ==
-             DetachedCtxEntryBlock) ||
+    DetachedCtxEntryBlock = GetDetachedCtx(OrigBB);
+    assert(((&(Caller->getEntryBlock()) == DetachedCtxEntryBlock) ||
+            pred_empty(DetachedCtxEntryBlock) ||
             DetachedCtxEntryBlock->getSinglePredecessor()) &&
            "Entry block of detached context has multiple predecessors.");
   }
@@ -3199,6 +3209,8 @@ llvm::InlineResult llvm::InlineFunction(CallBase &CB, InlineFunctionInfo &IFI,
   // instructions at the end of the current alloca list.
   {
     BasicBlock::iterator InsertPoint = DetachedCtxEntryBlock->begin();
+    if (isTaskFrameCreate(*InsertPoint))
+      InsertPoint++;
     for (BasicBlock::iterator I = FirstNewBlock->begin(),
          E = FirstNewBlock->end(); I != E; ) {
       AllocaInst *AI = dyn_cast<AllocaInst>(I++);

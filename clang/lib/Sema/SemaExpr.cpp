d45a38c6a680725ecc075fe51068f014bd1f55a5
TB Schardl <neboat@mit.edu>
2020-06-22 13:51:59 +0000

    [SemaExpr] Add semantic check that at most one spawn occurs among the arguments of a function call.

--- clang/lib/Sema/SemaExpr.cpp
+++ clang/lib/Sema/SemaExpr.cpp
@@ -6278,6 +6278,24 @@ bool Sema::CheckArgsForPlaceholders(MultiExprArg args) {
   return hasInvalid;
 }
 
+/// checkNumSpawnedExprs - Check that an appropriate number of spawned
+/// expressions appear in args.
+static bool checkNumSpawnedExprs(Sema &S, MultiExprArg args) {
+  unsigned count = 0;
+  for (size_t i = 0, e = args.size(); i != e; i++) {
+    if (isa<CilkSpawnExpr>(args[i])) {
+      ++count;
+      if (count > 1) {
+        if (count == 2)
+          S.Diag(args[i]->getBeginLoc(), diag::err_multiple_spawns);
+        else
+          S.Diag(args[i]->getBeginLoc(), diag::note_multiple_spawns);
+      }
+    }
+  }
+  return (count > 1);
+}
+
 /// If a builtin function has a pointer argument with no explicit address
 /// space, then it should be able to accept a pointer to any address
 /// space as input.  In order to do this, we need to replace the
@@ -6579,6 +6597,10 @@ ExprResult Sema::BuildCallExpr(Scope *Scope, Expr *Fn, SourceLocation LParenLoc,
     if (CheckInvalidBuiltinCountedByRef(Arg,
                                         BuiltinCountedByRefKind::FunctionArg))
       return ExprError();
+
+  // Check that we have at most one spawned argument.
+  if (checkNumSpawnedExprs(*this, ArgExprs))
+    return ExprError();
 
   if (getLangOpts().CPlusPlus) {
     // If this is a pseudo-destructor expression, build the call immediately.

1bf629267ab551656f148c1f7cb5b44292846a93
TB Schardl <neboat@mit.edu>
2020-07-24 15:37:04 +0000

    [Attributes] Add strand_pure LLVM attribute to identify functions that act like pure functions within a single strand of a Cilk or Tapir computation

--- llvm/include/llvm/Bitcode/LLVMBitCodes.h
+++ llvm/include/llvm/Bitcode/LLVMBitCodes.h
@@ -795,6 +795,7 @@ enum AttributeKindCodes {
   ATTR_KIND_CAPTURES = 102,
   ATTR_KIND_SANITIZE_CILK = 103,
   ATTR_KIND_STEALABLE = 104,
+  ATTR_KIND_STRAND_PURE = 105,
 };
 
 enum ComdatSelectionKindCodes {

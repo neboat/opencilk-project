2ee9dc5e775fb6ee58468b21326a34793be898a9
TB Schardl <neboat@mit.edu>
2020-12-04 13:12:09 +0000

    [CodeGenFunction] Remove a redundant check.

--- clang/lib/CodeGen/CodeGenFunction.cpp
+++ clang/lib/CodeGen/CodeGenFunction.cpp
@@ -1005,7 +1005,7 @@ void CodeGenFunction::StartFunction(GlobalDecl GD, QualType RetTy,
     Fn->addFnAttr("patchable-function", "prologue-short-redirect");
 
   // Add Cilk attributes
-  if (D && (getLangOpts().getCilk() != LangOptions::Cilk_none)) {
+  if (D && getLangOpts().getCilk() != LangOptions::Cilk_none) {
     if (D->getAttr<StrandPureAttr>())
       Fn->setStrandPure();
     if (D->getAttr<StealableAttr>())

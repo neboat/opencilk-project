b08d3a82cb7be38e46b576a4ab8f9cffa6171280
John F. Carr <jfc@mit.edu>
2022-06-21 13:57:40 -0400

    Clean up reducer callback order

--- clang/lib/CodeGen/CodeGenFunction.h
+++ clang/lib/CodeGen/CodeGenFunction.h
@@ -3986,8 +3986,8 @@ public:
   void emitAutoVarTypeCleanup(const AutoVarEmission &emission,
                               QualType::DestructionKind dtorKind);
   struct ReducerCallbacks {
-    Expr *Reduce;
     Expr *Identity;
+    Expr *Reduce;
     Expr *Destroy;
   };
 

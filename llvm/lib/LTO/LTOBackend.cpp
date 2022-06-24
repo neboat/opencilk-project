4f14837e1518eb7043dda50f29c535505f8c5b6d
TB Schardl <neboat@mit.edu>
2022-06-24 13:39:43 +0000

    [lld][LTO] Update lld and LTO to pass flags for Tapir lowering at link time.  Based on changes by George Stelle <stelleg@lanl.gov>.

--- llvm/lib/LTO/LTOBackend.cpp
+++ llvm/lib/LTO/LTOBackend.cpp
@@ -282,6 +282,10 @@ static void runNewPMPasses(const Config &Conf, Module &Mod, TargetMachine *TM,
 
   std::unique_ptr<TargetLibraryInfoImpl> TLII(
       new TargetLibraryInfoImpl(Triple(TM->getTargetTriple())));
+  TLII->setTapirTarget(Conf.TapirTarget);
+  TLII->setTapirTargetOptions(
+      std::make_unique<OpenCilkABIOptions>(Conf.OpenCilkABIBitcodeFile));
+  TLII->addTapirTargetLibraryFunctions();
   if (Conf.Freestanding)
     TLII->disableAllFunctions();
   FAM.registerPass([&] { return TargetLibraryAnalysis(*TLII); });
@@ -443,6 +447,10 @@ static void codegen(const Config &Conf, TargetMachine *TM,
 
   legacy::PassManager CodeGenPasses;
   TargetLibraryInfoImpl TLII(Triple(Mod.getTargetTriple()));
+  TLII.setTapirTarget(Conf.TapirTarget);
+  TLII.setTapirTargetOptions(
+      std::make_unique<OpenCilkABIOptions>(Conf.OpenCilkABIBitcodeFile));
+  TLII.addTapirTargetLibraryFunctions();
   CodeGenPasses.add(new TargetLibraryInfoWrapperPass(TLII));
   // No need to make index available if the module is empty.
   // In theory these passes should not use the index for an empty

099d8f6e7ccc915d90460ec1d6404c63d83b22a4
John F. Carr <jfc@mit.edu>
2023-06-05 15:34:56 -0400

    Allow hyperobject view lookup in overload resolution

--- clang/lib/Sema/SemaOverload.cpp
+++ clang/lib/Sema/SemaOverload.cpp
@@ -137,6 +137,7 @@ ImplicitConversionRank clang::GetConversionRank(ImplicitConversionKind Kind) {
       ICR_Exact_Match,
       ICR_Exact_Match,
       ICR_Exact_Match,
+      ICR_Exact_Match,
       ICR_Promotion,
       ICR_Promotion,
       ICR_Promotion,
@@ -5050,6 +5051,14 @@ TryReferenceInit(Sema &S, Expr *Init, QualType DeclType,
       T2 = Fn->getType();
   }
 
+  // OpenCilk: If the right hand side is a hyperobject, see if the
+  // left hand side wants the hyperobject or a view.
+  bool LookupView = false;
+  if (T2->isHyperobjectType() && !T1->isHyperobjectType()) {
+    LookupView = true;
+    T2 = T2.stripHyperobject();
+  }
+
   // Compute some basic properties of the types and the initializer.
   bool isRValRef = DeclType->isRValueReferenceType();
   Expr::Classification InitCategory = Init->Classify(S.Context);
@@ -5111,6 +5120,8 @@ TryReferenceInit(Sema &S, Expr *Init, QualType DeclType,
       //   in which case the implicit conversion sequence is a
       //   derived-to-base Conversion (13.3.3.1).
       SetAsReferenceBinding(/*BindsDirectly=*/true);
+      if (LookupView)
+        ICS.Standard.First = ICK_Hyperobject_To_View;
 
       // Nothing more to do: the inaccessibility/ambiguity check for
       // derived-to-base conversions is suppressed when we're
@@ -6001,6 +6012,7 @@ static bool CheckConvertedConstantConversions(Sema &S,
   case ICK_Array_To_Pointer:
   case ICK_Function_To_Pointer:
   case ICK_HLSL_Array_RValue:
+  case ICK_Hyperobject_To_View:
     llvm_unreachable("found a first conversion kind in Second");
 
   case ICK_Function_Conversion:

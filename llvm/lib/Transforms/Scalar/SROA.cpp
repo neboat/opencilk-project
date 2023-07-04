88004f89c1cc95386da762861aeaa0461f45b8b2
TB Schardl <neboat@mit.edu>
2023-07-04 17:43:49 +0000

    [SROA] Maintain Tapir task info as SROA modifies the CFG.  TODO: Add functionality to incrementally update Tapir task info analysis.

--- llvm/lib/Transforms/Scalar/SROA.cpp
+++ llvm/lib/Transforms/Scalar/SROA.cpp
@@ -5781,6 +5781,13 @@ std::pair<bool /*Changed*/, bool /*CFGChanged*/> SROA::runSROA(Function &F) {
         PromotableAllocas.set_subtract(DeletedAllocas);
         DeletedAllocas.clear();
       }
+
+      // Preserve TaskInfo by manually updating it based on the updated DT.
+      if (IterationCFGChanged && TI) {
+        // FIXME: Recalculating TaskInfo for the whole function is wasteful.
+        // Optimize this routine in the future.
+        TI->recalculate(F, DTU->getDomTree());
+      }
     }
 
     Changed |= promoteAllocas(F);
@@ -5815,6 +5822,7 @@ PreservedAnalyses SROAPass::run(Function &F, FunctionAnalysisManager &AM) {
   if (!CFGChanged)
     PA.preserveSet<CFGAnalyses>();
   PA.preserve<DominatorTreeAnalysis>();
+  PA.preserve<TaskAnalysis>();
   return PA;
 }
 
@@ -5862,6 +5870,7 @@ public:
     AU.addRequired<TaskInfoWrapperPass>();
     AU.addPreserved<GlobalsAAWrapperPass>();
     AU.addPreserved<DominatorTreeWrapperPass>();
+    AU.addPreserved<TaskInfoWrapperPass>();
   }
 
   StringRef getPassName() const override { return "SROA"; }

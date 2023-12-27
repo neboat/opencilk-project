9b5dea809e316a36bf0d8c083c5dfb5b64285eae
John F. Carr <jfc@mit.edu>
2023-12-27 15:07:25 -0500

    Call llvm.reducer.register to register hyperobject struct members

--- clang/lib/CodeGen/CGDecl.cpp
+++ clang/lib/CodeGen/CGDecl.cpp
@@ -1912,7 +1912,8 @@ void CodeGenFunction::emitZeroOrPatternForAutoVarInit(QualType type,
   }
 }
 
-bool CodeGenFunction::getReducer(const VarDecl *D, ReducerCallbacks &CB) {
+bool CodeGenFunction::getReducer(const DeclaratorDecl *D,
+                                 ReducerCallbacks &CB) {
   if (const HyperobjectType *H = D->getType()->getAs<HyperobjectType>()) {
     if (H->hasCallbacks()) {
       CB.Identity = H->getIdentity();
@@ -1937,7 +1938,7 @@ void CodeGenFunction::destroyHyperobject(CodeGenFunction &CGF, Address Addr,
   }
 }
 
-void CodeGenFunction::EmitReducerInit(const VarDecl *D,
+void CodeGenFunction::EmitReducerInit(const DeclaratorDecl *D,
                                       const ReducerCallbacks &C,
                                       llvm::Value *Addr) {
   RValue Identity = EmitAnyExpr(C.Identity);

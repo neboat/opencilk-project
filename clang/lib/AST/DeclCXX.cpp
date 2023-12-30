c8dfc8415c824cee29019a5d981c2d3889f5f568
John F. Carr <jfc@mit.edu>
2023-12-30 13:35:58 -0500

    Automatically register hyperobject struct members in C++

--- clang/lib/AST/DeclCXX.cpp
+++ clang/lib/AST/DeclCXX.cpp
@@ -1138,6 +1138,12 @@ void CXXRecordDecl::addedMember(Decl *D) {
         data().DefaultedCopyConstructorIsDeleted = true;
     }
 
+    if (T->isHyperobjectType()) {
+      data().HasIrrelevantDestructor = false;
+      data().HasTrivialSpecialMembers &= ~SMF_Destructor;
+      data().HasTrivialSpecialMembersForCall &= ~SMF_Destructor;
+    }
+
     if (isUnion() && !Field->isMutable()) {
       if (Field->hasInClassInitializer())
         data().HasUninitializedFields = false;

e2d7140543ac144d74e39b5e0edd7b9d11befc5b
John F. Carr <jfc@mit.edu>
2024-09-06 16:04:23 -0400

    Hyperobject struct members must be initialized

--- clang/lib/AST/DeclCXX.cpp
+++ clang/lib/AST/DeclCXX.cpp
@@ -1170,8 +1170,8 @@ void CXXRecordDecl::addedMember(Decl *D) {
       // suppress hyperobject registration.
       data().Aggregate = false;
       data().HasIrrelevantDestructor = false;
-      data().HasTrivialSpecialMembers &= ~SMF_Destructor;
-      data().HasTrivialSpecialMembersForCall &= ~SMF_Destructor;
+      data().HasTrivialSpecialMembers &= ~SMF_All;
+      data().HasTrivialSpecialMembersForCall &= ~SMF_All;
     }
 
     if (isUnion() && !Field->isMutable()) {

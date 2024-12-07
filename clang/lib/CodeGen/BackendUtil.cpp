571d7ad2602d835bdccea6569154bbdcd07a1998
TB Schardl <neboat@mit.edu>
2024-12-07 15:45:12 -0500

    [clang] Fix handling of Cilkscale tool variants, specifically cilkscale and cilkscale-instructions, to ensure instrumentation is inserted appropriately when using LLD with LTO.

--- clang/lib/CodeGen/BackendUtil.cpp
+++ clang/lib/CodeGen/BackendUtil.cpp
@@ -1094,7 +1094,7 @@ void EmitAssemblyHelper::RunOptimizationPipeline(
             });
         break;
       case LangOptions::CilktoolKind::Cilktool_Cilkscale_Benchmark:
-          PB.registerTapirLoopEndEPCallback(
+          PB.registerTapirLateEPCallback(
             [&PB](ModulePassManager &MPM, OptimizationLevel Level) {
               MPM.addPass(CSISetupPass(getCSIOptionsForCilkscaleBenchmark()));
               MPM.addPass(ComprehensiveStaticInstrumentationPass(

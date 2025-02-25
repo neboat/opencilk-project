c4bedfd0eaef5e15917be7c5be193b93b2dca851
John F. Carr <jfc@mit.edu>
2025-02-25 10:47:25 -0500

    Increase __cilk to 301

--- clang/lib/Frontend/InitPreprocessor.cpp
+++ clang/lib/Frontend/InitPreprocessor.cpp
@@ -1484,7 +1484,7 @@ static void InitializePredefinedMacros(const TargetInfo &TI,
     Builder.defineMacro("__cilk", "200");
     break;
   case LangOptions::Cilk_opencilk:
-    Builder.defineMacro("__cilk", "300");
+    Builder.defineMacro("__cilk", "301");
     break;
   }
 

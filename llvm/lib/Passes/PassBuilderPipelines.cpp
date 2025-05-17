5ebda72e8a68abfc2c9723aab622ed01cb2fd41b
TB Schardl <neboat@mit.edu>
2025-05-17 10:42:46 -0400

    [PassBuilderPipelines] Make sure LTO default pipeline performs Tapir lowering at O1.

--- llvm/lib/Passes/PassBuilderPipelines.cpp
+++ llvm/lib/Passes/PassBuilderPipelines.cpp
@@ -2183,6 +2183,16 @@ PassBuilder::buildLTODefaultPipeline(OptimizationLevel Level,
 
     invokeFullLinkTimeOptimizationLastEPCallbacks(MPM, Level);
 
+    // Add passes to run just before Tapir lowering.
+    invokeTapirLateEPCallbacks(MPM, Level);
+
+    // Lower Tapir if necessary
+    if (LowerTapir)
+      MPM.addPass(buildTapirLoweringPipeline(
+          Level, ThinOrFullLTOPhase::FullLTOPostLink));
+    else
+      invokeTapirLoopEndEPCallbacks(MPM, Level);
+
     // Emit annotation remarks.
     addAnnotationRemarksPass(MPM);
 

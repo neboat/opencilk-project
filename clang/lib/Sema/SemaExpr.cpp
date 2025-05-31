8fd920e7c10167df7b2987051df399ff045c4fb0
TB Schardl <neboat@mit.edu>
2025-05-30 23:17:12 -0400

    [SemaExpr] Fix crash with hyperobject lookups in complicated dependent contexts.

--- clang/lib/Sema/SemaExpr.cpp
+++ clang/lib/Sema/SemaExpr.cpp
@@ -2302,8 +2302,6 @@ Expr *Sema::BuildHyperobjectLookup(Expr *E, bool Pointer) {
   if (!HT)
     return E;
 
-  bool Difficult = CurContext->isDependentContext();
-
   QualType ResultType = HT->getElementType().withFastQualifiers(
       InputType.getLocalFastQualifiers());
   QualType Ptr = Context.getPointerType(ResultType);
@@ -2322,7 +2320,7 @@ Expr *Sema::BuildHyperobjectLookup(Expr *E, bool Pointer) {
   Expr *VarAddr;
   if (Pointer) {
     VarAddr = E;
-  } else if (Difficult) {
+  } else if (HT->getElementType()->isDependentType()) {
     ExprResult Address =
       BuildBuiltinCallExpr(Loc, Builtin::BI__builtin_addressof, E);
     assert(Address.isUsable());
@@ -2340,7 +2338,7 @@ Expr *Sema::BuildHyperobjectLookup(Expr *E, bool Pointer) {
   // Template expansion normally strips out implicit casts, so make this
   // explicit in C++.
   Expr *Casted = nullptr;
-  if (Difficult)
+  if (CurContext->isDependentContext())
     // Based on logic in CoroutineStmtBuilder::makeNewAndDeleteExpr()
     Casted =
         BuildCXXNamedCast(Loc, tok::kw_static_cast,
